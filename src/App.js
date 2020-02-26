d048c72d124417023b668587c3c40a97b3bf5c7f
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-02-25 20:52:54 -0500

    css and js upadatese

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from "./PlayerCard.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component {
+constructor(){
+  super();
+  this.sign = ["rock","scissor","paper"]
+  this.state = {
+    playerOne: "rock",
+    playerTwo: "scissor",
+  }
 }
 
-export default App;
+//this is where my methods will go :)
+playGame =() => {
+
+  this.setState(
+    {
+      playerOne: this.sign[Math.floor(Math.random() * 3)],
+      playerTwo: this.sign[Math.floor(Math.random() * 3)],
+    }
+  )
+
+}
+
+decideWinner = () => {
+    const playerOne = this.state.playerOne
+    const playerTwo = this.state.playerTwo
+
+    if (playerOne === playerTwo){
+        return "It is Tie!"
+    }
+    else if ((playerOne === "rock" && playerTwo === "scissor") ||
+    (playerOne === "paper" && playerTwo === "rock") ||
+    (playerOne=== "scissor" && playerTwo === "paper")) {
+    return "Player 1 Wins!"
+  } else{
+  return "Player 2 Wins!"
+  }
+}
+
+  render() {
+    return (
+<div className="style">
+<div>
+<PlayerCard sign={this.state.playerOne} />
+<PlayerCard sign={this.state.playerTwo} />
+</div>
+
+<div className="winner">
+{this.decideWinner()}
+</div>
+<button type="button" onClick={this.playGame}>Play the game :)</button>
+</div>
+
+)
+}
+};
+
+
+export default Game;
+
+
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
