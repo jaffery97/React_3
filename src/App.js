fbaaa8ab9d1375ec329d89afffe0eff0a8ef163c
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-02-27 14:17:47 -0500

    readme.md updated

--- src/App.js
+++ src/App.js
@@ -40,6 +40,10 @@ decideWinner = () => {
   }
 }
 
+
+
+
+
   render() {
     return (
 <div className="style">
@@ -64,33 +68,3 @@ decideWinner = () => {
 
 
 export default Game;
-
-
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
